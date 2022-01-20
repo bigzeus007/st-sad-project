d96f415bffbcdc6a8e95dd43ca85405c10e2512b
said.T <tarhisaid@gmail.com>
2022-01-20 22:22:29 +0100

    NEXT TESTING

--- pages/components/Test.js
+++ pages/components/Test.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Test(){
+export default function Test2(){
     return(
         <Link href="/">
             <a> RETOUR </a>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
-import Link from 'next/link'
-export default function Home(){
-    return (
-        <Link href={"/components/Test"}>
-        <a>HELLOOwwwwwww</a> 
-        </Link>
-     );
-};
+import Link from "next/link";
+export default function Home() {
+  return (
+    <>
+      <Link href={"/components/Test"}>
+        <a>HELLOOwwwwwww1</a>
+      </Link>
+
+      <Link href={"/components/Test2"}>
+        <a>HELLOOwwwwwww2</a>
+      </Link>
+      {` `}
+      <Link href={"/components/ExampleDialog"}>
+        <a>ExampleDialog</a>
+      </Link>
+    </>
+  );
+}
