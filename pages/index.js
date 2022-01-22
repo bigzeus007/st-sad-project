c57bb24a805d66712f35723175e8267bc385393d
said.T <tarhisaid@gmail.com>
2022-01-22 22:55:16 +0100

    STARTING ONE TOUCH APP

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href={"/components/Test"}>
-        <a>HELLOOwwwwwww1</a>
+      <Link href={"/components/Login"}>
+        <a>Login</a>
       </Link>
-
       <Link href={"/components/Test2"}>
         <a>HELLOOwwwwwww2</a>
       </Link>
-      {` `}
+      {" "}
       <Link href={"/components/ExampleDialog"}>
         <a>ExampleDialog</a>
       </Link>
