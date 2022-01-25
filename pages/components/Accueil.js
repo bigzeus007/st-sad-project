3c821d7eab023d7adaf0391329146d29619cc4c6
said.T <tarhisaid@gmail.com>
2022-01-25 23:35:53 +0100

    WORKING getDocs firebase

--- pages/components/Accueil.js
+++ pages/components/Accueil.js
@@ -9,7 +9,55 @@ import { NotificationTwoTone } from "@ant-design/icons";
 export default function Accueil(){
     
     const firestore=db;
-    
+
+    (async function getdata(){
+
+    const querySnapshot = await getDocs(collection(db, "users2"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+    })();
+
+
+     /* (async function test(){
+try {
+    const docRef = await addDoc(collection(firestore, "users2"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815,
+      createat: new Date(),
+      pic:"Hey you"
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+
+   // console.log(firestore)
+  })();
+  */
+
+
+
+
+    /*
+    FriendlyEats.prototype.getDocumentsInQuery = function(query, renderer) {
+  query.onSnapshot(function(snapshot) {
+    if (!snapshot.size) return renderer.empty(); // Display "There are no restaurants".
+
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        renderer.remove(change.doc);
+      } else {
+        renderer.display(change.doc);
+      }
+    });
+  });
+};
+
+*/
+/////////////////////////////////////////
    /* (async function test(){
 try {
     const docRef = await addDoc(collection(firestore, "users2"), {
@@ -46,12 +94,6 @@ try {
             <a> RETOUR </a>
         </Link>
 
-
+*/
         
 
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-
-*/
