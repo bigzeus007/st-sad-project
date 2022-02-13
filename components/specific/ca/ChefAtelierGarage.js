f197f4972083790902b0961b47c4d2911198665c
said.T <tarhisaid@gmail.com>
2022-02-13 13:36:27 +0100

    ATELIER FILTRED

--- components/specific/ca/ChefAtelierGarage.js
+++ components/specific/ca/ChefAtelierGarage.js
@@ -1,23 +1,56 @@
 import React from "react";
 import styled from "styled-components";
+import { techList } from "../tech/techList";
 
 export default function ChefAtelierGarage(){
 
     const GlobalAtelier= styled.div`
     position: relative;
-    height: 35vh;
+    height: 100%;
     width:100%;
-    display:flex;
-    justify-content:space-around;
-    margin:5px;
-    padding:5px;
+    display:grid;
+    grid-template-columns: 40vw 40vw;
+    
+    margin :10px 0px 0px 15px;
     border:2px blue solid;
+
+    `
+    const Atelier= styled.div`
+    position: relative;
+    display:grid;
+    grid-template-columns:32% 32% 32%;
+    grid-template-rows:auto;
+    padding:5px;
+    
+    justify-content:space-evenly;
+
+    height: 100%;
+    width:100%;
+    
+   
+   
+    border:2px blue solid;
+
+    `
+    const Button= styled.button`
+    box-shadow: 0px 5px 5px 0px;
+    border-radius: 0px 5px 5px 50px;
+    background-color:${props=>props.availability? "green":"red"};
 
     `
 
 
+
     return (
-        <GlobalAtelier>Global atelier</GlobalAtelier>
+        
+        <GlobalAtelier>
+            <Atelier className="express">
+                {techList.filter(tech=>(tech.atelierAffectation=="express")).map(filtredList=>(<Button availability={filtredList.availability}>{filtredList.nom}</Button>))}
+            </Atelier>
+            <Atelier className="mecanique"></Atelier>
+            <Atelier className="diagnostic"></Atelier>
+            <Atelier className="bodyCar"></Atelier>
+        </GlobalAtelier>
 
     )
 }
---
+++ components/specific/tech/techList.js
@@ -0,0 +1,156 @@
+
+
+
+export const techList=[
+    {
+        id:1,
+        nom:"Amine",
+        active:false,
+        atelierAffectation:"express",
+        availability:true,
+        workingDayDuration:"5h",
+        enAttente:2,
+        termines:3,
+
+
+},
+{
+    id:2,
+    nom:"Ilyass",
+    active:true,
+    atelierAffectation:"express",
+    availability:false,
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+
+},
+{
+    id:3,
+    nom:"JAWAD",
+    active:true,
+    atelierAffectation:"express",
+    availability:"true",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+
+},
+{
+    id:4,
+    nom:"RABIE",
+    active:true,
+    atelierAffectation:"mecanique",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+
+},
+{
+    id:5,
+    nom:"Redouane",
+    active:true,
+    atelierAffectation:"mecanique",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+
+},
+{
+    id:6,
+    nom:"Khalid",
+    active:true,
+    atelierAffectation:"mecanique",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+
+},
+{
+    id:7,
+    nom:"yassine",
+    active:true,
+    atelierAffectation:"electriue",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:8,
+    nom:"abdelmounime",
+    active:true,
+    atelierAffectation:"electriue",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:9,
+    nom:"youness",
+    active:true,
+    atelierAffectation:"bodyCar",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:10,
+    nom:"hassan",
+    active:true,
+    atelierAffectation:"bodyCar",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:11,
+    nom:"AADIL",
+    active:true,
+    atelierAffectation:"bodyCar",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:12,
+    nom:"ABDERRAZAK",
+    active:true,
+    atelierAffectation:"bodyCar",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+{
+    id:13,
+    nom:"abdelghafour",
+    active:true,
+    atelierAffectation:"bodyCar",
+    availability:"off",
+    workingDayDuration:"5h",
+    enAttente:2,
+    termines:3,
+
+},
+
+
+]
