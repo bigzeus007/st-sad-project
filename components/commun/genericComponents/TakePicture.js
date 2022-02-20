05e867b54c489a094f9659ace144cf95457ad01f
said.T <tarhisaid@gmail.com>
2022-02-20 22:29:54 +0100

    Webcam capture Working

--- components/commun/genericComponents/TakePicture.js
+++ components/commun/genericComponents/TakePicture.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useRef } from "react";
 import styled from "styled-components";
+
+
+
 export default function TakePicture() {
   const videoRef = useRef(null);
-  const pictureRef = useRef(null);
+  const photoRef = useRef(null);
   const [hasPhoto, setHasPhoto] = useState(false);
+  
+  
+
   const getVideo = () => {
+      const constraints={
+        audio: false,
+        video: {
+            facingMode: "environment",
+         
+        }
+      }
+      
     navigator.mediaDevices
-      .getUserMedia({
-        video: { width: 400, height: 550 },
-      })
+      .getUserMedia(constraints)
       .then((stream) => {
         let video = videoRef.current;
+        
         video.srcObject = stream;
+        
         video.play();
       })
       .catch((err) => {
@@ -20,53 +34,47 @@ export default function TakePicture() {
       });
   };
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
   const takePhoto = () => {
-    const width = 414;
-    const height = width / (16 / 9);
+    const width = 160;
+    const height = 320;
+    let photo = photoRef.current;
     let video = videoRef.current;
-    let photo = pictureRef.current;
     photo.width=width;
     photo.height=height;
 
     let ctx = photo.getContext("2d");
-    ctx.drawImage(video, 0, width, height);
+    ctx.drawImage(video, 0, 0, photo.width, photo.height);
+   
     setHasPhoto(true);
   };
 
-  const MainCarCard = styled.div`
-    
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext("2d");
+    ctx.clearRect(0, 0, photo.width, photo.height);
+    setHasPhoto(false);
+  };
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef]);
+
+  const [carStatus, setCarStatus] = useState("none");
+
   
-
-    height: (20*16/9)vw;
-
-  
-    width:20vw;
-  
-
-    border-radius: 50px;
-    box-shadow: 15px 15px 15px 10px black;
-
-    video{
-        width:100%;
-        height:100%;
-        
-    }
-
-  `;
 
   return (
-    <>
-      <MainCarCard>
-        <video ref={videoRef}></video>
-        <button onClick={takePhoto}>STart</button>
-      </MainCarCard>
-      <MainCarCard className={""}>
-        <canvas ref={pictureRef}></canvas>
-        <button onClick={()=>takePhoto}>teak picture</button>
-      </MainCarCard>
-    </>
+    <div style={{display:"flex",position:"relative", justifyItems:"center"}}>
+     
+        <div style={{display:"flex",width:"30vw",height:"35vh",border:"5px solid black"}}>
+          <canvas ref={photoRef}/>
+          <button style={{position:"absolute", }} onClick={closePhoto}>Annuler</button>
+        </div>
+      
+      <div style={{display:"flex",height:"16em"}}>
+        <video ref={videoRef} style={{borderRadius:"20%"}}/>
+        <button style={{position:"absolute"}}  onClick={takePhoto}>>Prendre photo</button>
+      </div>
+    </div>
   );
 }
