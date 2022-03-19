c4a43ab42f81bbf650677c92c2cc3eeb90b37004
said.T <tarhisaid@gmail.com>
2022-03-19 21:34:25 +0100

    WORKING ON TECH CARD

--- components/commun/genericComponents/TechCard.js
+++ components/commun/genericComponents/TechCard.js
@@ -1,52 +1,102 @@
 import React from "react";
-
+import Image from "next/image";
 import styled from "styled-components";
 import { auth } from "../../../firebase";
-import techPhoto from "../../../public/images/carPicture.jpg"
+import techPhoto from "../../../public/images/carPicture.jpg";
 
 import { useState, useRef, useEffect } from "react";
 
-const CardTechStyled = styled.section`
-  display: inline-block;
-  word-wrap: break-word;
-  width:20%;
-  height:100%;
-  border-radius: 50px;
-  box-shadow: 15px 15px 15px 10px black;
-  box-sizing: border-box;
-  .techIdentity{
-    width:80%;
-    height:50%;
+const CardTechStyled = styled.div`
+ width:20%;
+ height:25%;
+ min-width:100px;
+ max-height:25vh;
+ 
+ 
+  
+  .techCard {
+    position: relative;
+    border: 1px blue solid;
+  }
 
-  }
-  .techAffected{
-    width:90%;
-    height:10%;
-    margin-left:5%;
-
-  }
-  .techPhoto{
-    width:100%;
-    height:100%;
-  }
-  .techParkStat{
+  .progressIndicators {
+    position: absolute;
     display:flex;
-    justify-content:space-evenly;
+    background-color: red;
+    top: 0;
+    right: 0;
+    width: 10%;
+    height: 100%;
+  }
+  .progressBar{
+    width:50%;
+    height:100%;
+
+  }
+  .tauDeChargeRestant{
+    height:100%;
+    background-color:greenyellow;
+  }
+  .tauOcuupationJournee{
+    height:100%;
+    background-color:green;
   }
 
+  .techIdentity {
+    width: 90%;
+    height: 50%;
+  }
+
+  .techAffected {
+    position:absolute;
+    top:0;
+    background-color:transparent;
+    padding-top:25%;
+    color:white;
+    width: 100%;
+    height: 100%;
+    font-size: 2vw;
+    :hover{
+      color:green;
+    }
+  }
+
+  .techParkStat {
+    height: 40%;
+    display: flex;
+    justify-content: space-evenly;
+  }
 `;
-
-
 
 export default function TechCard() {
   const props = {
     photo: techPhoto,
     nom: "SAID",
     availability: false,
+    doingStartWorking:9.5,
     toDo: 2,
     done: 3,
+    hToDo:3,
+    hDone:1.5,
+    hDoing:1,
+    workDayHours:9,
+
     cardsHistory: ["cardID1", "cardID2"],
   };
+
+  let DoingProgress="30%";
+  let txOccupation="50%";
+
+  // Future use For adding progress bar
+  // const date=new Date();
+  // let minutes = date.getMinutes();
+  // let hour = date.getHours();
+  // let nowTimeConverter=minutes/60+hour;
+
+
+
+
+
   //user check
   const checkUserEmail = auth.currentUser.email;
   function checkProfilTech(checking) {
@@ -55,26 +105,28 @@ export default function TechCard() {
   return (
     <CardTechStyled>
       <div className="techCard">
-        <section className="progressIndicators">
-          <div className="tauDeChargeRestant progressBar"></div>
-          <div className="tauDeChargeRestant progressBar"></div>
-        </section>
         <section
           className="techInfos"
-          style={{ backgroundColor: `${props.availability ? "red" : "green"}` }}
+          style={{
+            backgroundColor: `${props.availability ? "red" : "yellow"}`,
+          }}
         >
           <div className="techIdentity">
-            {
-              (!props.photo ? (
-                <p>{props.nom}</p>
-              ) : (
-                <img
-                  className="techPhoto"
-                  src={props.photo}
-                  alt="techPhoto"
-               />
-              ))
-            }
+            {!props.photo ? (
+              <p>{props.nom}</p>
+            ) : (
+              <Image
+                // width={100}
+                // height={100}
+                className="techPhoto"
+                src={props.photo}
+                alt="techPhoto"
+              />
+            )}
+          </div>
+          <div className="progressIndicators">
+            <div  className="progressBar"><div className="tauDeChargeRestant" style={{height:`${DoingProgress}`}}></div></div>
+            <div className="progressBar"><div className="tauOcuupationJournee"style={{height:`${txOccupation}`}}></div></div>
           </div>
           <button className="techAffected">Ajouter</button>
           <div className="techParkStat">
